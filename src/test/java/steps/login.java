de6f7259bc37e7f70d3bedc071788d48f44da096
johnkristiadi <johnkristiadi@gmail.com>
2024-11-04 20:09:49 +0700

    update step login

--- src/test/java/steps/login.java
+++ src/test/java/steps/login.java
@@ -1,19 +1,33 @@
 package steps;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 
 public class login {
 	
 	WebDriver driver;
 	
-	@Given("i navigate to web")
-	public void iNavigateToWeb() {
+	@Given("I open website {string}")
+	public void iOpenWebsite(String url) {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		driver  = new ChromeDriver();
-		driver.get("https://www.saucedemo.com/");
+		driver = new ChromeDriver();
+		driver.navigate().to(url);
+		driver.manage().window().maximize();
 	}
-
+	@And("I input username {string}")
+	public void iInputUsername(String userName) {
+		driver.findElement(By.id("user-name")).sendKeys(userName);
+	}
+	@And("I input password {string}")
+	public void iInputPassword(String password) {
+		driver.findElement(By.id("password")).sendKeys(password);
+	}
+	@And("I click button login")
+	public void iClickButtonLogin() {
+		driver.findElement(By.id("login-button")).click();
+	}
 }
